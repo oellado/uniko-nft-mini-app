9b0c61a51d3bf092fb8ef9dc16c3e8520628beb9
oellado <o.dalle@slmail.me>
2025-06-12 15:44:47 +0200

    Fix TypeScript build error by removing unused React import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useAccount, useConnect, useDisconnect, useWriteContract, useWaitForTransactionReceipt } from 'wagmi';
 import { parseEther } from 'viem';
 
